ef69a620c407dd19b2df5b288c54b95f1e7e100b
aider <1muzafarov.a.i20@gmail.com>
2021-11-12 01:08:52 +0300

    2.5 dinamic menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-  <template>
-
+<template>
   <v-app>
-  <v-navigation-drawer app v-model="drawer" <v-list-item>
+  <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">
             КИПУ
@@ -16,38 +16,49 @@
 
       <v-list dense>
         <v-list-item-group color="primary">
-          <v-list-item>
+          <v-list-item 
+          v-for="link in links" 
+          :key="link.title"
+          >
             <v-list-item-icon>
-              <v-icon>mdi-cake-variant</v-icon>
+              <v-icon>{{ link.icon }}</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title>Link One</v-list-item-title>
+              <v-list-item-title>{{ link.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
-></v-navigation-drawer> 
+  </v-navigation-drawer> 
   <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link OneLink One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+       <v-btn 
+       v-for="link in links" 
+       :key="link.title" 
+       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn> 
     </v-toolbar-items>
   </v-app-bar>
   <v-main>
-    <router-view></router-view> 
+    <!--<router-view></router-view>--> 
   </v-main>
   </v-app>
 </template>
+
 <script>
 export default {
-  
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+      {title:"Login", icon:"mdi-lock", url:"/login"},
+      {title:"Registration", icon:"mdi-face", url:"/registration"},
+      {title:"Orders", icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+      {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
+      {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+      ]
     }
   }
 }
-</script>
+ 
