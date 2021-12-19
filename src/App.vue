868d842118ff1fe4d13b43123470740bd5caa4fc
aider <1muzafarov.a.i20@gmail.com>
2021-12-19 20:59:50 +0300

    7-7.2

--- src/App.vue
+++ src/App.vue
@@ -33,13 +33,20 @@
   </v-navigation-drawer> 
   <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>
+      <router-link 
+to="/" 
+tag="span" 
+class="pointer"
+>Home</router-link>
+</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn 
-       v-for="link in links" 
-       :key="link.title"
-       :to="link.url"  
-       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn> 
+  <v-btn 
+  v-for="link in links" 
+  :key="link.title"
+  :to="link.url"  
+  text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn> 
     </v-toolbar-items>
   </v-app-bar>
   <v-main>
